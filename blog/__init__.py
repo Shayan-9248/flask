cc0857da02262b0783489047cabf0a50ea841bdb
shayan <shayan.aimoradii@gmail.com>
2021-02-11 14:05:35 +0330

    models

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_sqlachemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../blog.db'
+db = SQLAlchemy(app)
 
 
 
