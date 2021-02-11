04988d571170efeb190b298e5872f5ae9c27d7b4
shayan <shayan.aimoradii@gmail.com>
2021-02-11 15:03:59 +0330

    .gitignore

--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,11 @@ from datetime import datetime
 
 
 class User(db.Model):
-	id = db.Column(db.integer, primary_key=True)
+	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(30), unique=True, nullable=False)
 	email = db.Column(db.String(67), unique=True, nullable=False)
 	password = db.Column(db.String(70), nullable=False)
-	posts = db.relationshop('Post', backref='author', lazy=True)
+	posts = db.relationship('Post', backref='author', lazy=True)
 
 	def __repr__(self):
 		return f'{self.__class__.__name__}({self.id} - {self.username})'
@@ -16,11 +16,11 @@ class User(db.Model):
 
 
 class Post(db.Model):
-	id = db.Column(db.integer, primary_key=True)
+	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(77), nullable=False)
 	date = db.Column(db.DateTime, default=datetime.now, nullable=False)
 	body = db.Column(db.Text, nullable=False)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 	def __repr__(self):
-		return f'{self.__class__.__name__}({self.id} - {self.title[:27]} - {self.date})'
+		return f'{self.__class__.__name__}({self.id} - {self.title[:27]} - {self.date.strftime})'
