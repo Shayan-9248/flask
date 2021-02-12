638d56840f6974275d4fea2737320a645c8c6c58
shayan <shayan.aimoradii@gmail.com>
2021-02-12 15:49:07 +0330

    form validation and error

--- blog/forms.py
+++ blog/forms.py
@@ -1,17 +1,28 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from .models import User
 
 
 
 class SignupForm(FlaskForm):
 	username = StringField('Username',
-	 validators=[DataRequired(), Length(min=4, max=27)])
+	 validators=[DataRequired(), Length(min=3, max=27)])
 	email = StringField('Email', validators=[DataRequired(), Email()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	confirm_password = PasswordField('Confirm_Password',
-	 validators=[DataRequired(), EqualTo('password')])
+	 validators=[DataRequired(), EqualTo('password', message='Passwords must match!')])
 	submit = SubmitField('Sign-up')
+
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user:
+			raise ValidationError('this username is already exists')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user:
+			raise ValidationError('this email is already exists')
 
 
 class LoginForm(FlaskForm):
